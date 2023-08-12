98263b065ca74d445e55faa6a398405fae525762
Lee <minmin1243@naver.com>
2023-08-12 19:34:35 +0900

    feat: ts study 2차 커밋

--- dist/practice.js
+++ dist/practice.js
@@ -1,8 +1,18 @@
 "use strict";
 var message = 'hello typeScript';
-console.log(message);
 var count = 0;
-count += 1;
-// count = "1" // type 오류
+count += 1; // count = "1" // type 오류
 var numbers = [1, 2, 3]; // 숫자 배열
 var messages = ['hello', 'world']; // 문자열 배열
+var color = 'red';
+color = 'yellow'; // color = 'green'; //red, orange, yellow에 포함되어 있지 않으면 type 오류
+var sumArray = function (numbers) {
+    return numbers.reduce(function (acc, cur) { return acc + cur; }, 0);
+};
+var arrSum = sumArray([1, 2, 3, 4, 5]);
+var returnNothing = function () {
+    // 아무것도 반환하지 않는다면 타입 void
+    console.log('No Return');
+};
+console.log(arrSum);
+returnNothing();
--- src/practice.ts
+++ src/practice.ts
@@ -1,13 +1,24 @@
 const message: string = 'hello typeScript';
-console.log(message);
 
 let count = 0;
-count += 1;
-// count = "1" // type 오류
+count += 1; // count = "1" // type 오류
 
 const numbers: number[] = [1, 2, 3]; // 숫자 배열
 const messages: string[] = ['hello', 'world']; // 문자열 배열
 
 let color: 'red' | 'orange' | 'yellow' = 'red';
-color = 'yellow';
-// color = 'green'; //red, orange, yellow에 포함되어 있지 않으면 type 오류
+color = 'yellow'; // color = 'green'; //red, orange, yellow에 포함되어 있지 않으면 type 오류
+
+const sumArray = (numbers: number[]) => {
+  return numbers.reduce((acc, cur) => acc + cur, 0);
+};
+
+const arrSum = sumArray([1, 2, 3, 4, 5]);
+
+const returnNothing = (): void => {
+  // 아무것도 반환하지 않는다면 타입 void
+  console.log('No Return');
+};
+
+console.log(arrSum);
+returnNothing();
